e4a85387f068bbeadcf531edd88c1f235480e2a0
Milad Farazian <mfarazia@ucsc.edu>
2020-02-12 19:59:31 -0800

    Attempt to fix 500 error

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
+/*$(function(){
+	$('.btn btn-outline-secondary btn-lg').click(function(){
+		$('html,body').animate({
+			scrollTop:('#work').offset.top
+		}
+		'slow'
+		)
+	})
+})*/
