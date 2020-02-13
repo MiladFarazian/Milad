4d7045d100c482c4e8539d2b72c54f8d2e6e1fa0
Milad Farazian <mfarazia@ucsc.edu>
2020-02-12 20:48:54 -0800

    Uh Oh

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
-/*$(function(){
-	$('.btn btn-outline-secondary btn-lg').click(function(){
-		$('html,body').animate({
-			scrollTop:('#work').offset.top
-		}
-		'slow'
-		)
-	})
-})*/
+$(function(){
+	$("btn btn-outline-secondary btn-lg").click(function(){
+		if(this.hash){
+			var hash = this.hash.substr(1);
+			console.log(hash);
+			
+			var $toElement = $("a[name="+hash+"]");
+			var toPosition = $toElement.position().top;
+			
+			$("body,html").animate({
+				scrollTop:toPostion
+			},2000,"easeOutExpo");
+			
+			return false;
+		}		
+	});
+	if(location.hash){
+		var hash = location.hash;
+		window.scroll(0.1);
+		$("a[href="+hash+"]").click();
+
+	}
+})
