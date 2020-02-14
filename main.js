51ce2017b0f99add9e87a2ae3da2cdb5a82fb7a7
Milad Farazian <mfarazia@ucsc.edu>
2020-02-13 16:58:59 -0800

    Fixed Scroll

--- main.js
+++ main.js
@@ -2,25 +2,10 @@ $(document).ready(function(){
   $('.header').height($(window).height());
 })
 $(function(){
-	$("btn btn-outline-secondary btn-lg").click(function(){
-		if(this.hash){
-			var hash = this.hash.substr(1);
-			console.log(hash);
-			
-			var $toElement = $("a[name="+hash+"]");
-			var toPosition = $toElement.position().top;
-			
-			$("body,html").animate({
-				scrollTop:toPostion
-			},2000,"easeOutExpo");
-			
-			return false;
-		}		
-	});
-	if(location.hash){
-		var hash = location.hash;
-		window.scroll(0.1);
-		$("a[href="+hash+"]").click();
-
-	}
+	$('#work-expereince').click(function(){
+		$('html,body').animate({
+			scrollTop:('work-experience').offset().top
+		}, 'slow')
+	})
 })
+
